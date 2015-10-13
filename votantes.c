717b1b61dc71a60383ce670c122e73e4d612e1ad
Lucas Gabriel <rexonav8xd@hotmail.com>
2015-10-12 23:18:50 -0300

    Update and rename votantes.h to votantes.c

---
+++ votantes.c
@@ -0,0 +1,28 @@
+#include "mesa.h"
+#include <string.h>
+#include "votantes.h"
+
+bool persona_valida_a_votar( char *dni, char *tipoDNI, mesa_t* mesa_escrutinio )
+{
+	bool pertenece = false;
+	
+	lista_iter_t* iterador = lista_iter_crear(mesa_escrutinio->personas_a_votar);
+	int iDNI = atoi(dni);
+	votante_t* tmp;
+	
+	while( !lista_iter_al_final(iterador) )
+	{
+		tmp = lista_iter_ver_actual(iterador);
+		
+		if( !strcmp(tmp->tipoDNI, tipoDNI) && (tmp->numeroDNI == iDNI) )
+			pertenece = true;
+			
+		if( pertenece )
+			return true;
+		
+		lista_iter_avanzar(iterador);
+	}
+	lista_iter_destruir(iterador);
+	
+	return false;
+}
